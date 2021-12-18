cd74251585acf5aa6c875885d87cb304cb6ac9f7
moonwatcher00 <moonwatcher1018@gmail.com>
2021-12-19 08:31:11 +0900

    Update firstcommit.java

--- firstcommit.java
+++ firstcommit.java
@@ -1,10 +1,20 @@
 import javax.swing.*;
+import java.awt.FlowLayout;
 
 	public class firstcommit extends JFrame{
 		public firstcommit() {
 			setSize(300,200);
 			setTitle("My Frame");
-			setVisible(true);
+			
+            setLayout(new FlowLayout());
+            JButton button = new JButton("버튼");
+            JButton button2 = new JButton("버튼2");
+            add(button);
+            add(button2);
+            setVisible(true);
+
+             
+            
 		}
 		public static void main(String[]args) {
 			firstcommit f = new firstcommit();
