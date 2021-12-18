2d133a80d752b3bcb8c76ecf846369e815231f3d
moonwatcher00 <moonwatcher1018@gmail.com>
2021-12-19 07:25:56 +0900

    Create firstcommit.java
     첫번째 커밋.

---
+++ firstcommit.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+	public class firstcommit extends JFrame{
+		public firstcommit() {
+			setSize(300,200);
+			setTitle("My Frame");
+			setVisible(true);
+		}
+		public static void main(String[]args) {
+			firstcommit f = new firstcommit();
+		}
+	
+	}
